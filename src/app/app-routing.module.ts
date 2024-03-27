935bc4879ad74bfc5611bebdc9d95b5ca59167d5
gitwangan <wangang@usc.edu>
2024-03-26 21:47:44 -0700

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {TodosComponent} from "./components/todos/todos.component";
+import {TodoDetailsComponent} from "./components/todo-details/todo-details.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'todos', pathMatch:'full'},
+  {path:'todos', component: TodosComponent}, {path: 'todos/:id', component: TodoDetailsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodosComponent } from './components/todos/todos.component';
+import { TodoDetailsComponent } from './components/todo-details/todo-details.component';
+import { HttpClientModule} from "@angular/common/http";
+import { FormatTodoTitlePipe } from './pipes/format-todo-title.pipe';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodosComponent,
+    TodoDetailsComponent,
+    FormatTodoTitlePipe
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        FormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
